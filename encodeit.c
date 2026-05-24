a5526292aafc0fda1423fe86aea52298092e14bf
sk20052001 <sanjeev@pdx.edu>
2026-05-23 19:45:29 -0700

    completed reg-reg and imm-reg

---
+++ encodeit.c
@@ -0,0 +1,84 @@
+
+//
+// simple encoding examples for IA-32 validtion project
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "ia32_encode.h"
+
+// globals to aid debug to start
+volatile char *mptr=0,*next_ptr=0;
+int num_inst=0;
+
+main(int argc, char *argv[])
+{
+
+	int ibuilt=0;
+
+
+	/* allocate buffer to build instructions into */
+
+	mptr=malloc(MAX_INSTR_BYTES);
+	next_ptr=mptr;                  // init next_ptr
+
+	ibuilt=build_instructions();  	// build instructions
+
+	fprintf(stderr,"generation program complete, instructions generated: %d\n",ibuilt);
+}
+
+//
+// Routine:  build_instructions
+//
+// Description:
+//
+// INPUT: none yet
+// 
+// OUTPUT: return the number of instructions built
+// 
+int build_instructions() {
+
+	// example instruction generation..
+
+    // test move cl into bl
+    next_ptr=build_mov_register_to_register(ISZ_1, REG_ECX, REG_EBX, next_ptr);
+	num_inst++;
+
+	fprintf(stderr,"next ptr is now 0x%lx\n", (long) next_ptr);
+
+
+	// test move cx into bx
+	next_ptr=build_mov_register_to_register(ISZ_2, REG_ECX, REG_EBX, next_ptr);
+	num_inst++;
+
+	fprintf(stderr,"next ptr is now 0x%lx\n", (long) next_ptr);
+
+
+	// test move edx into edi
+	next_ptr=build_mov_register_to_register(ISZ_4, REG_EDX, REG_EDI, next_ptr);
+	num_inst++;
+
+	fprintf(stderr,"next ptr is now 0x%lx\n", (long) next_ptr);
+
+    // test move 0x34 into al
+    next_ptr=build_mov_imm_to_register(ISZ_1, 0x34,REG_EAX, next_ptr);
+	num_inst++;
+
+	fprintf(stderr,"next ptr is now 0x%lx\n", (long) next_ptr);
+
+
+	// test move 0x3456 into bx
+	next_ptr=build_mov_imm_to_register(ISZ_2, 0x3456 ,REG_EBX, next_ptr);
+	num_inst++;
+
+	fprintf(stderr,"next ptr is now 0x%lx\n", (long) next_ptr);
+
+
+	// test move 0x23456789 into edi
+	next_ptr=build_mov_imm_to_register(ISZ_4, 0x23456789, REG_EDI, next_ptr);
+	num_inst++;
+
+	fprintf(stderr,"next ptr is now 0x%lx\n", (long) next_ptr);
+
+	return (num_inst);
+}
---
+++ include/ia32_encode.h
@@ -0,0 +1,186 @@
+/*
+ * GENERAL Instruction Format
+ *
+ * -----------------------------------------------------------------
+ * | Instructoin    |   Opcode | ModR/M | Displacement | Immediate |
+ * | Prefixess       |          |        |              |           |
+ * -----------------------------------------------------------------
+ *
+ *  7  6  5   3  2   0
+ * --------------------
+ * | Mod | Reg** | R/M |
+ * --------------------
+ */ 
+
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * definition we need to support these functions.
+ *
+ * see Table 2-2 in SDM for register/MODRM encode usage
+ *
+ *
+ */
+#define PREFIX_16BIT   0x66
+#define BASE_MODRM     0xc0
+#define REG_SHIFT      0x3
+#define MODRM_SHIFT    0x6
+#define RM_SHIFT       0x0
+#define REG_MASK       0x7
+#define RM_MASK        0x7
+#define MOD_MASK       0x3
+
+// register defs based on MOD RM table
+#define REG_EAX        0x0
+#define REG_ECX        0x1
+#define REG_EDX        0x2
+#define REG_EBX        0x3
+#define REG_ESP        0x4
+#define REG_EBP        0x5
+#define REG_ESI        0x6
+#define REG_EDI        0x7
+
+// byte offset
+#define BYTE1_OFF      0x1
+#define BYTE2_OFF      0x2
+#define BYTE3_OFF      0x3
+#define BYTE4_OFF      0x4
+
+// ISIZE (instruction size in bytes, for move example 2byte = 16bit)
+
+#define ISZ_1         0x1
+#define ISZ_2         0x2
+#define ISZ_4         0x4
+#define ISZ_8         0x8
+
+
+// code generation defines
+
+#define MAX_INSTR_BYTES 10000
+/*
+ * Function: build_mov_register_to_register
+ *
+ * Description:
+ *
+ * Inputs: 
+ *
+ *  short mov_size               :  size of the move being requested
+ *  int   src_reg                :  register sorce encoding 
+ *  int   dest_reg               :  destination reg of move
+ *  volatile char *tgt_addr      :  starting memory address of where to store instruction
+ *
+ * Output: 
+ *
+ *  returns adjusted address after encoding instruction
+ *
+ */
+
+
+static inline volatile char *build_mov_register_to_register(short mov_size, int src_reg, int dest_reg, volatile char *tgt_addr)
+{
+	// for 16 bit mode we need to treat it special because it requires a prefix
+
+	if (mov_size == 2) {
+		(*tgt_addr ++) = PREFIX_16BIT;
+	}
+
+	// now lets look at each size and determine which opcode required
+
+	switch(mov_size)  {
+
+	case 1: 
+		(*(short *) tgt_addr) = ((BASE_MODRM) + (dest_reg << REG_SHIFT) + src_reg) << 8 | 0x8a;
+		 tgt_addr += BYTE2_OFF;
+		 break;
+
+	case 2:  
+        (*(short *) tgt_addr) = ((BASE_MODRM) + (dest_reg << REG_SHIFT) + src_reg) << 8 | 0x8b;
+		 tgt_addr += BYTE2_OFF;
+		 break;
+
+	case 4: 
+		(*(short *) tgt_addr) = ((BASE_MODRM) + (dest_reg << REG_SHIFT) + src_reg) << 8 | 0x8b;
+		 tgt_addr += BYTE2_OFF;
+		 break;
+
+	default:
+		 fprintf(stderr,"ERROR: Incorrect size (%d) passed to register to register move\n", mov_size);
+		 return (NULL);
+
+	}
+			
+        return(tgt_addr);
+}
+
+static inline volatile char *build_mov_imm_to_register(short mov_size, int imm_value, int dest_reg, volatile char *tgt_addr)
+{
+	// for 16 bit mode we need to treat it special because it requires a prefix
+
+	// if (mov_size == 2) {
+	// 	(*tgt_addr ++) = PREFIX_16BIT;
+	// }
+
+	// now lets look at each size and determine which opcode required
+
+	switch(mov_size)  {
+
+	case 1: 
+		(*(short *) tgt_addr) = imm_value << 8 | (0xb0 + dest_reg);
+		 tgt_addr += BYTE2_OFF;
+		 break;
+
+	case 2:  
+        (*(int *) tgt_addr) = (imm_value << 16) | ((0xb8 + dest_reg) << 8) | PREFIX_16BIT;
+		 tgt_addr += BYTE4_OFF;
+		 break;
+
+	case 4: 
+        (*tgt_addr ++) = 0xb8 + dest_reg;
+		(*(int *) tgt_addr) = imm_value;
+		 tgt_addr += BYTE4_OFF;
+		 break;
+
+	default:
+		 fprintf(stderr,"ERROR: Incorrect size (%d) passed to immediate to register move\n", mov_size);
+		 return (NULL);
+
+	}
+			
+        return(tgt_addr);
+}
+
+static inline volatile char *build_mov_register_to_memory(short mov_size, int src_reg, int dest_reg, volatile char *tgt_addr)
+{
+	// for 16 bit mode we need to treat it special because it requires a prefix
+
+	if (mov_size == 2) {
+		(*tgt_addr ++) = PREFIX_16BIT;
+	}
+
+	// now lets look at each size and determine which opcode required
+
+	switch(mov_size)  {
+
+	case 1: 
+		(*(short *) tgt_addr) = ((BASE_MODRM) + (dest_reg << REG_SHIFT) + src_reg) << 8 | 0x8a;
+		 tgt_addr += BYTE2_OFF;
+		 break;
+
+	case 2:  
+        (*(short *) tgt_addr) = ((BASE_MODRM) + (dest_reg << REG_SHIFT) + src_reg) << 8 | 0x8b;
+		 tgt_addr += BYTE2_OFF;
+		 break;
+
+	case 4: 
+		(*(short *) tgt_addr) = ((BASE_MODRM) + (dest_reg << REG_SHIFT) + src_reg) << 8 | 0x8b;
+		 tgt_addr += BYTE2_OFF;
+		 break;
+
+	default:
+		 fprintf(stderr,"ERROR: Incorrect size (%d) passed to register to register move\n", mov_size);
+		 return (NULL);
+
+	}
+			
+        return(tgt_addr);
+}
