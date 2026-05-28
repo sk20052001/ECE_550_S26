d7bb94027031c27ae19c89ac0169fb2024b1fa6b
sk20052001 <sanjeev@pdx.edu>
2026-05-27 18:29:48 -0700

    completed register to memory without displacement

--- encodeit.c
+++ encodeit.c
@@ -40,6 +40,8 @@ int build_instructions() {
 
 	// example instruction generation..
 
+
+    // Register to Register
     // test move cl into bl
     next_ptr=build_mov_register_to_register(ISZ_1, REG_ECX, REG_EBX, next_ptr);
 	num_inst++;
@@ -60,15 +62,17 @@ int build_instructions() {
 
 	fprintf(stderr,"next ptr is now 0x%lx\n", (long) next_ptr);
 
+
+    // Immediate to Register
     // test move 0x34 into al
-    next_ptr=build_mov_imm_to_register(ISZ_1, 0x34,REG_EAX, next_ptr);
+    next_ptr=build_mov_imm_to_register(ISZ_1, 0x34, REG_EAX, next_ptr);
 	num_inst++;
 
 	fprintf(stderr,"next ptr is now 0x%lx\n", (long) next_ptr);
 
 
 	// test move 0x3456 into bx
-	next_ptr=build_mov_imm_to_register(ISZ_2, 0x3456 ,REG_EBX, next_ptr);
+	next_ptr=build_mov_imm_to_register(ISZ_2, 0x3456, REG_EBX, next_ptr);
 	num_inst++;
 
 	fprintf(stderr,"next ptr is now 0x%lx\n", (long) next_ptr);
@@ -76,6 +80,28 @@ int build_instructions() {
 
 	// test move 0x23456789 into edi
 	next_ptr=build_mov_imm_to_register(ISZ_4, 0x23456789, REG_EDI, next_ptr);
+	num_inst++;
+
+	fprintf(stderr,"next ptr is now 0x%lx\n", (long) next_ptr);
+
+
+    // Register to Memory
+    // test move dl into [al]
+    next_ptr=build_mov_register_to_memory(ISZ_1, REG_EDX, REG_EAX, next_ptr);
+	num_inst++;
+
+	fprintf(stderr,"next ptr is now 0x%lx\n", (long) next_ptr);
+
+
+	// test move cx into [si]
+	next_ptr=build_mov_register_to_memory(ISZ_2, REG_ECX, REG_ESI, next_ptr);
+	num_inst++;
+
+	fprintf(stderr,"next ptr is now 0x%lx\n", (long) next_ptr);
+
+
+	// test move eax into [edi]
+	next_ptr=build_mov_register_to_memory(ISZ_4, REG_EAX, REG_EDI, next_ptr);
 	num_inst++;
 
 	fprintf(stderr,"next ptr is now 0x%lx\n", (long) next_ptr);
