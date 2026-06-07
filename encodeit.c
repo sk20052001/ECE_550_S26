330abef43e273c2f9f9d3423b6d1a84c99ebe295
Amey <ameyk1510@gmail.com>
2026-06-06 18:53:20 -0700

    excluded EBX from destination reg

--- encodeit.c
+++ encodeit.c
@@ -3,14 +3,13 @@
 // simple encoding example fr IA-32 validation project
 //
 // project_part2
-// 
+//
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/mman.h>
 
 #include "ia32_encode.h"
-
 
 #ifndef PAGESIZE
 #define PAGESIZE 4096
@@ -21,10 +20,10 @@
 #endif
 
 // globals to aid debug to start
-volatile char *mptr=0,*next_ptr=0, *mdptr=0;
-int num_inst=0;
-int inst_goal=25;
-unsigned int inst_seed=1;
+volatile char *mptr = 0, *next_ptr = 0, *mdptr = 0;
+int num_inst = 0;
+int inst_goal = 25;
+unsigned int inst_seed = 1;
 
 // declarations for starting test
 typedef int (*funct_t)();
@@ -45,7 +44,7 @@ static inline void emit_random_instruction(void);
 int main(int argc, char *argv[])
 {
 
-	int ibuilt=0,rc=0;
+	int ibuilt = 0, rc = 0;
 
 	if (argc > 1)
 	{
@@ -67,51 +66,47 @@ int main(int argc, char *argv[])
 	/* allocate buffer to perform stores and loads to, and set permissions  */
 
 	mdptr = (volatile char *)mmap(
-		(void *) 0,
-		(MAX_DATA_BYTE+PAGESIZE-1),
+		(void *)0,
+		(MAX_DATA_BYTE + PAGESIZE - 1),
 		PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_SHARED,
-		0, 0
-		);
+		0, 0);
 
-	if(mdptr == MAP_FAILED) { 
-		printf("data mptr allocation failed\n"); 
-		exit(1); 
+	if (mdptr == MAP_FAILED)
+	{
+		printf("data mptr allocation failed\n");
+		exit(1);
 	}
-
 
 	/* allocate buffer to build instructions into, and set permissions to allow execution of this memory area */
 
 	mptr = (volatile char *)mmap(
-		(void *) 0,
-		(MAX_INSTR_BYTES+PAGESIZE-1),
+		(void *)0,
+		(MAX_INSTR_BYTES + PAGESIZE - 1),
 		PROT_READ | PROT_WRITE | PROT_EXEC,
 		MAP_ANONYMOUS | MAP_SHARED,
-		0, 0
-		);
+		0, 0);
 
-	if(mptr == MAP_FAILED) { 
-		printf("instr  mptr allocation failed\n"); 
-		exit(1); 
+	if (mptr == MAP_FAILED)
+	{
+		printf("instr  mptr allocation failed\n");
+		exit(1);
 	}
 
-	next_ptr=mptr;                  // init next_ptr
+	next_ptr = mptr; // init next_ptr
 
-	ibuilt=build_instructions();  	// build instructions
+	ibuilt = build_instructions(); // build instructions
 
 	/* ok now that I built the critters, time to execute them */
 
-	start_test=(funct_t) mptr;
+	start_test = (funct_t)mptr;
 	executeit(start_test);
 
-
-	fprintf(stderr,"generation program complete, %d instructions generated, and executed\n",ibuilt);
+	fprintf(stderr, "generation program complete, %d instructions generated, and executed\n", ibuilt);
 
 	// clean up the allocation before getting out
 
-	munmap((caddr_t)mdptr,(MAX_DATA_BYTE+PAGESIZE-1));
-	munmap((caddr_t)mptr,(MAX_INSTR_BYTES+PAGESIZE-1));
-
-
+	munmap((caddr_t)mdptr, (MAX_DATA_BYTE + PAGESIZE - 1));
+	munmap((caddr_t)mptr, (MAX_INSTR_BYTES + PAGESIZE - 1));
 }
 
 //
@@ -131,7 +126,7 @@ static inline int rand_range(int min_value, int max_value)
 //
 // Description:
 //
-// Pick one of the instruction families the professor asked for.
+// Pick one of the instruction families.
 //
 static inline int pick_instruction_kind(void)
 {
@@ -147,7 +142,7 @@ static inline int pick_instruction_kind(void)
 //
 static inline short pick_mov_size(void)
 {
-	static const short sizes[] = { ISZ_1, ISZ_2, ISZ_4, ISZ_8 };
+	static const short sizes[] = {ISZ_1, ISZ_2, ISZ_4, ISZ_8};
 	return sizes[rand_range(0, 3)];
 }
 
@@ -161,8 +156,8 @@ static inline short pick_mov_size(void)
 //
 static inline int pick_general_reg(short mov_size)
 {
-	static const int regs_word_dword_qword[] = { REG_EAX, REG_ECX, REG_EDX, REG_ESI, REG_EDI };
-	static const int regs_byte[] = { REG_EAX, REG_ECX, REG_EDX, REG_ESI };
+	static const int regs_word_dword_qword[] = {REG_EAX, REG_ECX, REG_EDX, REG_ESI, REG_EDI};
+	static const int regs_byte[] = {REG_EAX, REG_ECX, REG_EDX, REG_ESI};
 
 	if (mov_size == ISZ_1)
 	{
@@ -195,7 +190,7 @@ static inline int pick_dest_reg(short mov_size, int source_reg)
 {
 	int dest_reg = pick_general_reg(mov_size);
 
-	while (dest_reg == source_reg)
+	while ((dest_reg == source_reg) || (dest_reg == REG_EBX))
 	{
 		dest_reg = pick_general_reg(mov_size);
 	}
@@ -212,7 +207,7 @@ static inline int pick_dest_reg(short mov_size, int source_reg)
 //
 static inline long pick_displacement(void)
 {
-	static const long disps[] = { 0, 8, 32 };
+	static const long disps[] = {0, 8, 32};
 	return disps[rand_range(0, 2)];
 }
 
@@ -236,9 +231,9 @@ static inline long pick_immediate(short mov_size)
 	case ISZ_8:
 	default:
 		return (long)((((unsigned long long)rand() << 48) ^
-			((unsigned long long)rand() << 32) ^
-			((unsigned long long)rand() << 16) ^
-			(unsigned long long)rand()));
+					   ((unsigned long long)rand() << 32) ^
+					   ((unsigned long long)rand() << 16) ^
+					   (unsigned long long)rand()));
 	}
 }
 
@@ -279,31 +274,31 @@ static inline void emit_random_instruction(void)
 	}
 
 	num_inst++;
-	fprintf(stderr, "next ptr is now 0x%lx\n", (long) next_ptr);
+	fprintf(stderr, "next ptr is now 0x%lx\n", (long)next_ptr);
 }
 
 /*
  * Function: executeit
- * 
+ *
  * Description:
  *
- * This function will start executing at the function address passed into it 
+ * This function will start executing at the function address passed into it
  * and return an integer return value that will be used to indicate pass(0)/fail(1)
  *
- * INTPUTs:  funct_t start_addr :      function pointer 
+ * INTPUTs:  funct_t start_addr :      function pointer
  *
  * Returns:  int                :      0 for pass, 1 for fail
- */   
-int executeit(funct_t start_addr) 
+ */
+int executeit(funct_t start_addr)
 {
 
-	volatile int i,rc=0;
+	volatile int i, rc = 0;
 
-	i=0;
+	i = 0;
 
-	rc=(*start_addr)();
+	rc = (*start_addr)();
 
-	return(0);
+	return (0);
 }
 
 //
@@ -318,13 +313,13 @@ static inline volatile char *add_headeri(volatile char *tgt_addr)
 {
 #if defined(__x86_64__) || defined(_M_X64)
 	// Build a real x86_64 stack frame first so the generated code can return cleanly.
-	(*tgt_addr++) = 0xc8;                         // enter
-	(*(short *)tgt_addr) = 2048;                  // stack size
+	(*tgt_addr++) = 0xc8;		 // enter
+	(*(short *)tgt_addr) = 2048; // stack size
 	tgt_addr += BYTE2_OFF;
-	(*tgt_addr++) = 0x00;                         // nesting level
+	(*tgt_addr++) = 0x00; // nesting level
 
 	// Save the callee-saved registers we may rely on inside the generated block.
-	(*tgt_addr++) = 0x53;                         // push rbx
+	(*tgt_addr++) = 0x53; // push rbx
 	tgt_addr = build_push_reg(REG_R12, 1, tgt_addr);
 	tgt_addr = build_push_reg(REG_R13, 1, tgt_addr);
 	tgt_addr = build_push_reg(REG_R14, 1, tgt_addr);
@@ -334,10 +329,10 @@ static inline volatile char *add_headeri(volatile char *tgt_addr)
 	tgt_addr = build_mov_imm_to_register(ISZ_8, (long)mdptr, REG_EBX, tgt_addr);
 #else
 	// On 32-bit x86, save everything up front and use ebx as the data base pointer.
-	(*tgt_addr++) = 0xc8;                         // enter
-	(*(short *)tgt_addr) = 2048;                  // stack size
+	(*tgt_addr++) = 0xc8;		 // enter
+	(*(short *)tgt_addr) = 2048; // stack size
 	tgt_addr += BYTE2_OFF;
-	(*tgt_addr++) = 0x00;                         // nesting level
+	(*tgt_addr++) = 0x00; // nesting level
 
 	tgt_addr = build_pusha(tgt_addr);
 	tgt_addr = build_mov_imm_to_register(ISZ_4, (long)mdptr, REG_EBX, tgt_addr);
@@ -362,15 +357,15 @@ static inline volatile char *add_endi(volatile char *tgt_addr)
 	tgt_addr = build_pop_reg(REG_R14, 1, tgt_addr);
 	tgt_addr = build_pop_reg(REG_R13, 1, tgt_addr);
 	tgt_addr = build_pop_reg(REG_R12, 1, tgt_addr);
-	(*tgt_addr++) = 0x5b;                         // pop rbx
+	(*tgt_addr++) = 0x5b; // pop rbx
 
-	(*tgt_addr++) = 0xc9;                         // leave
-	(*tgt_addr++) = 0xc3;                         // ret
+	(*tgt_addr++) = 0xc9; // leave
+	(*tgt_addr++) = 0xc3; // ret
 #else
 	// 32-bit x86 uses popa to match the push-all prologue.
 	tgt_addr = build_popa(tgt_addr);
-	(*tgt_addr++) = 0xc9;                         // leave
-	(*tgt_addr++) = 0xc3;                         // ret
+	(*tgt_addr++) = 0xc9; // leave
+	(*tgt_addr++) = 0xc3; // ret
 #endif
 
 	return (tgt_addr);
@@ -382,10 +377,11 @@ static inline volatile char *add_endi(volatile char *tgt_addr)
 // Description:
 //
 // INPUT: none yet
-// 
+//
 // OUTPUT: returns the number of instructions built
-// 
-int build_instructions() {
+//
+int build_instructions()
+{
 
 	// Start with the standard function-style prologue.
 	next_ptr = add_headeri(next_ptr);
